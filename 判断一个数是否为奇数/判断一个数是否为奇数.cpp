b439f9a3716b185d6ea5f8a99143b094f843da97
範刷 <38583625+LXH824@users.noreply.github.com>
2022-02-11 19:57:19 +0800

    Add files via upload

---
+++ 判断一个数是否为奇数/判断一个数是否为奇数.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+int main()
+{
+	int x, y;
+	printf("请输入数字：");
+	scanf_s("%d", &x);
+	if (1 == x % 2)
+	{
+		printf("这个数是奇数\n");
+	}
+	else
+	{
+		printf("这个数是偶数\n");
+	}
+return 0;
+}
